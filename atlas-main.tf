472104fde14f80bc51467372b7a9c92862d6efaa
rachnap-12 <45859337+rachnap-12@users.noreply.github.com>
2022-01-24 16:34:50 +0530

    Update atlas-main.tf

--- atlas-main.tf
+++ atlas-main.tf
@@ -3,3 +3,22 @@ resource "mongodbatlas_project" "atlas-project" {
   org_id = var.atlas_org_id
   name = var.atlas_project_name
 }
+
+# Create a Database Password
+resource "random_password" "db-user-password" {
+  length = 16
+  special = true
+  override_special = "_%@"
+}
+
+# Create a Database User
+resource "mongodbatlas_database_user" "db-user" {
+  username = "galaxy-read"
+  password = random_password.db-user-password.result
+  project_id = mongodbatlas_project.atlas-project.id
+  auth_database_name = "admin"
+  roles {
+    role_name     = "read"
+    database_name = "${var.atlas_project_name}-db"
+  }
+}
