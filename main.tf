7fcb0b1642e2b9092466ff9029eb3e4bc8cdc40a
rachnap-12 <45859337+rachnap-12@users.noreply.github.com>
2022-01-24 16:22:26 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,9 @@
+# Define Terraform provider
+terraform {
+  required_version = ">= 0.12"
+}
+# Define the MongoDB Atlas Provider
+provider "mongodbatlas" {
+  public_key  = var.atlas_public_key
+  private_key = var.atlas_private_key
+}
