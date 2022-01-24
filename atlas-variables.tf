6ccbec800561710a642e5094ca612c2297f4cb50
rachnap-12 <45859337+rachnap-12@users.noreply.github.com>
2022-01-24 16:28:34 +0530

    Create atlas-variables.tf

---
+++ atlas-variables.tf
@@ -0,0 +1,10 @@
+# Atlas Organization ID 
+variable "atlas_org_id" {
+  type        = string
+  description = "Atlas organization id"
+}
+# Atlas Project Name
+variable "atlas_project_name" {
+  type        = string
+  description = "Atlas project name"
+}
